00ed6e4c86b41d5f343f05fcde6285467885ffe0
류태연 <junsei8318@naver.com>
2023-03-28 10:16:46 +0900

    주석 추가

--- src/main/java/com/benit/svc2/Svc2Application.java
+++ src/main/java/com/benit/svc2/Svc2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Svc2Application {
 
 	public static void main(String[] args) {
+		//First Service 실행
 		SpringApplication.run(Svc2Application.class, args);
 	}
 
